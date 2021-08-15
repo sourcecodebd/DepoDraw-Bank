0a52f140f3b65505ad7d13f5e20ea8b4e14a9980
sourcebd <nafimahmud1996@gmail.com>
2021-08-15 10:43:55 +0600

    making site done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # DepoDraw Bank
+## Site-Link
+https://sourcecodebd.github.io/DepoDraw-Bank/
+
+## Author 
+[Nafi Mahmud][author]
+
+[author]: https://sourcecodebd.github.io/nafi.com/
 This is a simple deposit-withdraw calculating app made by HTML, CSS, JavaScript. Hope beginner would found this helpful!
-Site-Link: https://sourcecodebd.github.io/DepoDraw-Bank/
---
+++ js/bank.js
@@ -0,0 +1,65 @@
+// handle deposit and withdraw event
+// deposit
+document.getElementById('deposit-submit').addEventListener('click', function () {
+    if ((!/^[0-9]+$/.test(document.getElementById('deposit-input').value))) {
+        const warning = document.getElementById('deposit-warning');
+        warning.innerText = 'Please Enter a number!';
+        setTimeout(function () {
+            warning.innerText = '';
+        }, 3000)
+    }
+    else if ((document.getElementById('deposit-input').value.length) >= 15) {
+        console.log((document.getElementById('deposit-input').value.length));
+        const warning = document.getElementById('deposit-warning');
+        warning.innerText = 'Please Enter a number less than 15 digits!';
+        setTimeout(function () {
+            warning.innerText = '';
+        }, 3000)
+    }
+    else {
+        const depositInput = document.getElementById('deposit-input');
+        const newDeposit = depositInput.value;
+
+        const depositAmount = document.getElementById('deposit-amount');
+        const previousDeposit = depositAmount.innerText;
+        depositAmount.innerText = parseFloat(previousDeposit) + parseFloat(newDeposit);
+        depositInput.value = '';
+
+        // total balance
+        const balance = document.getElementById('balance');
+        balance.innerText = parseFloat(balance.innerText) + parseFloat(newDeposit);
+    }
+})
+
+// withdraw
+document.getElementById('withdraw-submit').addEventListener('click', function () {
+    if ((!/^[0-9]+$/.test(document.getElementById('withdraw-input').value))) {
+        const warning = document.getElementById('withdraw-warning');
+        warning.innerText = 'Please Enter a number!';
+        setTimeout(function () {
+            warning.innerText = '';
+        }, 3000)
+    }
+    else if ((document.getElementById('withdraw-input').value.length) >= 15) {
+        console.log((document.getElementById('withdraw-input').value.length));
+        const warning = document.getElementById('withdraw-warning');
+        warning.innerText = 'Please Enter a number less than 15 digits!';
+        setTimeout(function () {
+            warning.innerText = '';
+        }, 3000)
+    }
+    else {
+        console.log((document.getElementById('withdraw-input').value.length))
+        const withdrawInput = document.getElementById('withdraw-input');
+        const newWithdraw = withdrawInput.value;
+
+        const withdrawAmount = document.getElementById('withdraw-amount');
+        const previousWithdraw = withdrawAmount.innerText;
+        withdrawAmount.innerText = parseFloat(previousWithdraw) + parseFloat(newWithdraw);
+        withdrawInput.value = '';
+
+        // total balance
+        const balance = document.getElementById('balance');
+        balance.innerText = parseFloat(balance.innerText) - parseFloat(newWithdraw);
+    }
+})
